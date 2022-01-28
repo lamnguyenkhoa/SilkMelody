588e3daed8b2471ae4d75880c5f1a9459aadadcb
NKLam <khoalamvn@gmail.com>
2022-01-28 09:30:35 +0700

    Finished all levels for demo

--- Assets/Interactable/SilkbindSandal/SilkbindSandal.cs
+++ Assets/Interactable/SilkbindSandal/SilkbindSandal.cs
@@ -11,6 +11,9 @@ public class SilkbindSandal : MonoBehaviour
     public GameObject tutorial;
     public GameObject promt;
     private InputMaster inputMaster;
+    private AudioSource sound;
+    private CanvasGroup canvasGroup;
+    public float fadeSpeed = 0.3f;
 
     private void Awake()
     {
@@ -22,6 +25,8 @@ public class SilkbindSandal : MonoBehaviour
     {
         inputMaster = new InputMaster();
         inputMaster.Enable();
+        sound = GetComponent<AudioSource>();
+        canvasGroup = tutorial.GetComponent<CanvasGroup>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -39,9 +44,19 @@ public class SilkbindSandal : MonoBehaviour
 
     private IEnumerator DoubleJumpTutorial(Player player)
     {
+        canvasGroup.alpha = 0;
         player.disableControlCounter += 1;
         promt.SetActive(false);
         tutorial.SetActive(true);
+        sound.Play();
+
+        // Fade in tutorial
+        while (canvasGroup.alpha < 1)
+        {
+            canvasGroup.alpha += fadeSpeed * Time.deltaTime;
+            yield return null;
+        }
+
         yield return new WaitForSeconds(tutorialMinTime);
         promt.SetActive(true);
         InputAction jumpAction = inputMaster.Gameplay.Jump;
@@ -49,6 +64,14 @@ public class SilkbindSandal : MonoBehaviour
         {
             yield return null;
         }
+
+        // Fade out
+        while (canvasGroup.alpha > 0)
+        {
+            canvasGroup.alpha -= fadeSpeed * Time.deltaTime;
+            yield return null;
+        }
+
         tutorial.SetActive(false);
         player.disableControlCounter -= 1;
         StopAllCoroutines();
